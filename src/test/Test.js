72caf824181b358d54d3f77ea66c5498849e7507
godseop <godseop@gmail.com>
2022-08-08 22:24:11 +0900

    공통컴포넌트 적용중

--- src/test/Test.js
+++ src/test/Test.js
@@ -1,24 +1,26 @@
-import ListComponent from "../common/ListComponent";
 import {useRef, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import DetailComponent from "../common/DetailComponent";
+import GridComponent from "../common/GridComponent";
+import ColumnComponent from "../common/ColumnComponent";
 
 const Test = () => {
+
+  const [condition, setCondition] = useState({
+    CONDITION1: '',
+    CONDITION2: '',
+    CONDITION3: '',
+  });
 
   const [viewCondition, setViewCondition] = useState(true);
   const [viewList, setViewList] = useState(false);
   const [viewDetail, setViewDetail] = useState(false);
+  const [viewPopup, setViewPopup] = useState(false);
 
   const listRef = useRef(null);
   const detailRef = useRef(null);
 
   const navigate = useNavigate();
-
-  const [condition, setCondition] = useState({
-    WH_CD: '',
-    STRR_ID: '',
-    CUST_CD: '',
-  });
 
   const main = () => {
     navigate(-1);
@@ -86,7 +88,11 @@ const Test = () => {
       </div>
 
       <div className={viewList ? 'visible' : 'hidden'}>
-        <ListComponent ref={listRef} onSelect={onSelect}/>
+        <GridComponent ref={listRef} onSelect={onSelect}>
+          <ColumnComponent field='' headerText=''/>
+          <ColumnComponent field='' headerText=''/>
+          <ColumnComponent field='' headerText=''/>
+        </GridComponent>
         <br/>
         <button onClick={back}>뒤로</button>
       </div>
