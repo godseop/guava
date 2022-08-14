6a53737dedf97b1227bb7c4c9c2f5ad1cf143ac7
godseop <godseop@gmail.com>
2022-08-14 14:28:07 +0900

    로그인 화면 작업#1

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Main from "./test/Main";
+import Login from "./test/Login";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/login" element={<Login/>}/>
           <Route path="/" element={<Main/>}/>
         </Routes>
       </BrowserRouter>
---
+++ src/test/Login.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Login = (props) => {
+
+  return (
+    <div>
+      <h1>로그인</h1>
+
+      <input type='text' name='ID'/>
+      <input type='password' name='PASSWORD'/>
+    </div>
+  );
+}
+
+export default Login;
